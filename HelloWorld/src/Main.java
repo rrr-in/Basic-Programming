13c6b77b4d8f8b31242fdb29c1e9349556caa5e0
rrr-in <karinafilzaa@gmail.com>
2024-03-06 12:33:44 +0700

    hello world

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!, Hi Karina Filza, Programming is so much fun!");
+        System.out.println("Hello world!, Hi Karina Filza Aafiyah, Programming is so much fun!");
     }
 }
